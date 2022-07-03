5bbe5c39e7b4b29e4fa6263a653d758f8e25e0dd
kushar77 <kusharora339@gmail.com>
2022-07-03 11:01:27 -0700

    minor ui changes made

--- frontend/app/src/main/java/com/example/unistat/MainActivity.java
+++ frontend/app/src/main/java/com/example/unistat/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
--- frontend/app/src/main/java/com/example/unistat/UserProfileActivity.java
+++ frontend/app/src/main/java/com/example/unistat/UserProfileActivity.java
@@ -2,9 +2,11 @@ package com.example.unistat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
