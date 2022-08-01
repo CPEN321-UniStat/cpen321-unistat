9057af261e51983fee176a23fdd629009460d510
manek11 <54968278+manek11@users.noreply.github.com>
2022-08-01 15:56:16 -0700

    search bar fixed

--- frontend/app/src/main/java/com/example/unistat/statscardview/ViewStatsActivity.java
+++ frontend/app/src/main/java/com/example/unistat/statscardview/ViewStatsActivity.java
@@ -8,19 +8,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -110,19 +115,6 @@ public class ViewStatsActivity extends AppCompatActivity {
             }
         });
 
-
-
-        filterAutoComplete.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if (!b){
-                    filterAutoComplete.setEnabled(true);
-                    filterAutoComplete.setFocusable(false);
-                    filterAutoComplete.setFocusableInTouchMode(false);
-                }
-            }
-        });
-
         // Get data on the basis of item clicked
         filterAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -206,6 +198,27 @@ public class ViewStatsActivity extends AppCompatActivity {
 
 
         }
+
+
+    /**
+     * Clear focus on touch outside for all EditText inputs.
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof AutoCompleteTextView) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
 
     private void sortStatsByEntranceScore(boolean b) {
         int filterTextLength = filterAutoComplete.getText().toString().length();
