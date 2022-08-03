7ba669d47d252f21675f36b639e34c1e0d034b68
QuinnCarroll <62186145+QuinnCarroll@users.noreply.github.com>
2022-08-03 00:56:45 -0400

    Better coverage

--- backend/integration_testing/__tests__/integration.test.js
+++ backend/integration_testing/__tests__/integration.test.js
@@ -779,6 +779,26 @@ describe("GET /meetingsById", () => {
     })
 })
 
+// Tests for getting optimal meetingsById
+describe("GET /optimalMeetings", () => {
+    test("Get meetings for a valid user", async () => {
+        await process.nextTick(() => { });
+
+        const res = await request(app).get("/optimalMeetings/kusharora339@gmail.com")
+                                .set("startDay", 26).set("startMonth", 6)
+                                .set("endDay", 29).set("endMonth", 6)
+                                .set("year", 2022).set("weekloadermonth", 6)
+        expect(res.statusCode).toBe(200)
+        // expect to get the meeting that was inputted above
+        expect(JSON.parse(res.text).meetings.some(meeting => {
+            if (meeting.mId == meetingID) {
+                return true;
+            }
+            return false;
+        }))
+    })
+})
+
 // Tests for updating meeting status
 describe("PUT /meetings", () => {
     test("Update meeting status with an invalid status", async () => {
