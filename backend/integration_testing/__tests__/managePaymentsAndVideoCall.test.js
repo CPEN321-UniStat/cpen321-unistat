85a9b8cdc739be65585772fbdbcb90b25a7cac2a
QuinnCarroll <62186145+QuinnCarroll@users.noreply.github.com>
2022-08-04 00:31:36 -0400

    Mocked shceduleJob so payment has higher coverage

--- backend/integration_testing/__tests__/managePaymentsAndVideoCall.test.js
+++ backend/integration_testing/__tests__/managePaymentsAndVideoCall.test.js
@@ -2,6 +2,21 @@ const request = require('supertest')
 const {app, server} = require('../../server')
 const db = require("../../database/connect")
 const client = db.client
+const { changeTesting } = require('../../meetings/meetingHandlers')
+
+
+const meetingID = 12345;
+const { handlePayment } = require('../../payments/paymentHandlers')
+jest.mock('node-schedule');
+const schedule = require('node-schedule');
+
+schedule.scheduleJob.mockImplementation(() => {
+    handlePayment(meetingID)
+})
+
+beforeAll( () => {
+    changeTesting();
+})
 
 afterAll( () => {
     // Close the server instance after each test
@@ -9,33 +24,50 @@ afterAll( () => {
     client.close()
 })
 
-const meetingID = 12345;
-
-// // // Tests for createZoomMeeting
-// describe("POST /createZoomMeeting", () => {
-//     test("Creates a Zoom meeting", async () => {
-//         await process.nextTick(() => { });
-//         const res = await request(app).post("/createZoomMeeting").send({
-//             "meetingTopic": "Test Meeting",
-//             "meetingStartTime": "2022-08-11'T'11:05:00",
-//             "meetingEndTime": "2022-08-11'T'12:05:00",
-//             "mId": meetingID
-//         })
-//         await process.nextTick(() => { });
-//         expect(res.statusCode).toBe(200)
-//     })
-// })
-
 // Tests for updating meeting logs
 describe("PUT /updateMeetingLog", () => {
     test("Update meeting logs with valid mId", async () => {
-        const res = await request(app).put("/updateMeetingLog").send({
+        await process.nextTick(() => { });
+        var res = await request(app).put("/updateMeetingLog").send({
             "mId": meetingID,
             "meetingLog": {
                 "timestamp": "2022-07-09T11:00:00",
-                "userEmail": "manekgujral11@gmail.com",
+                "userEmail": "kusharora339@gmail.com",
                 "isMentor": true,
                 "action": "JOINED"
+            }
+        })
+        expect(res.statusCode).toBe(200)
+
+        res = await request(app).put("/updateMeetingLog").send({
+            "mId": meetingID,
+            "meetingLog": {
+                "timestamp": "2022-07-09T11:01:00",
+                "userEmail": "manekgujral11@gmail.com",
+                "isMentor": false,
+                "action": "JOINED"
+            }
+        })
+        expect(res.statusCode).toBe(200)
+
+        res = await request(app).put("/updateMeetingLog").send({
+            "mId": meetingID,
+            "meetingLog": {
+                "timestamp": "2022-07-09T11:55:00",
+                "userEmail": "manekgujral11@gmail.com",
+                "isMentor": false,
+                "action": "LEFT"
+            }
+        })
+        expect(res.statusCode).toBe(200)
+
+        res = await request(app).put("/updateMeetingLog").send({
+            "mId": meetingID,
+            "meetingLog": {
+                "timestamp": "2022-07-09T11:58:00",
+                "userEmail": "kusharora339@gmail.com",
+                "isMentor": true,
+                "action": "LEFT"
             }
         })
         expect(res.statusCode).toBe(200)
@@ -52,5 +84,20 @@ describe("PUT /updateMeetingLog", () => {
             }
         })
         expect(res.statusCode).toBe(400)
+    })
+})
+
+// // // Tests for createZoomMeeting
+describe("POST /createZoomMeeting", () => {
+    test("Creates a Zoom meeting", async () => {
+        await process.nextTick(() => { });
+        const res = await request(app).post("/createZoomMeeting").send({
+            "meetingTopic": "Test Meeting",
+            "meetingStartTime": "2022-08-11'T'11:05:00",
+            "meetingEndTime": "2022-08-11'T'12:05:00",
+            "mId": meetingID
+        })
+        await process.nextTick(() => { });
+        expect(res.statusCode).toBe(200)
     })
 })
