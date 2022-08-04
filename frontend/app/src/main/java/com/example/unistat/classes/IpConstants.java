413c0109984a7e7a7fef4c867d6bfed45764a9fa
Vijeeth <vijeethvp@gmail.com>
2022-08-03 22:28:42 -0400

    fixed frontend test

--- frontend/app/src/main/java/com/example/unistat/classes/IpConstants.java
+++ frontend/app/src/main/java/com/example/unistat/classes/IpConstants.java
@@ -4,7 +4,7 @@ public class IpConstants {
 
     public static final String serverIP = "51.142.77.51";
     public static final String localIP = "10.0.2.2";
-    public static final String URL = "http://" + "10.0.2.2" + ":8081/";
+    public static final String URL = "http://" + localIP + ":8081/";
 
     private IpConstants() {
         // No instantiation
