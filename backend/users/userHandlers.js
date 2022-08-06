5f04d1b50c691898043e03a8c86e015beac58fcf
Vijeeth <vijeethvp@gmail.com>
2022-08-05 22:16:03 -0400

    tried fixing try block

--- backend/users/userHandlers.js
+++ backend/users/userHandlers.js
@@ -47,15 +47,15 @@ const client = db.client;
  * @param {*} res 
  */
 const handleUserEntry = async (req, res) => {
-
-    if (req.body.Token == undefined || req.body.firebase_token == undefined) {
-        res.status(400).send(JSON.stringify({
-            "status": "Cannot create user with undefined body"
-        }))
-        return
-    }
-    
+  
     try {
+        if (req.body.Token == undefined || req.body.firebase_token == undefined) {
+            res.status(400).send(JSON.stringify({
+                "status": "Cannot create user with undefined body"
+            }))
+            return
+        }
+        
         var alreadyExists = await storeGoogleUserData(req.body.Token, req.body.firebase_token);
         // console.log("exists: " + alreadyExists);
         res.status(200).send(JSON.stringify({
@@ -65,10 +65,6 @@ const handleUserEntry = async (req, res) => {
         console.log(error)
         res.status(400).send(JSON.stringify(error));
     }
-    
-}
-
-const  func = async (token, firebase_token, res) => {
     
 }
 
